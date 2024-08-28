ee4d818ce4eb8ba8079c2471cdd2baaf60303312
Rohit <flux.amv25@gmail.com>
2024-08-28 06:51:33 +0530

    Added LogicalOperators.js

---
+++ Chp2/LogicalOperators.js
@@ -0,0 +1,12 @@
+// Understanding the logical Operators
+
+let x = 7 , y = 8 , z = 10
+
+let result = x > y && x < z // nothing simple logic just understand truth table
+console.log(result)
+
+let result1 = x > y || y < z
+console.log(result1)
+
+let n = !result // does opposite of result
+console.log("Original : " + result ," , Manipulated : " +  n)
