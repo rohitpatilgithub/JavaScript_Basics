fa93363dc7dc38eb0d4b7d14d69c83eab3e3774c
Rohit <flux.amv25@gmail.com>
2024-08-28 06:47:26 +0530

    Added Constant.js

---
+++ Chp1/Constant.js
@@ -0,0 +1,9 @@
+// Understanding importance of Constants
+
+let radius = 5
+const pi = 3.14 
+let area = pi * radius * radius
+
+// pi = 5 this cannot be changed as we declared it as const
+
+console.log("Area is : " + area)
